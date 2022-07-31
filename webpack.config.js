dda3643613eaf2c1c0f98251cdf7e636ce181ae6
Krishna <libraryismyparadise@gmail.com>
2022-07-31 21:39:14 +0545

    Add and style game.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPartialsPlugin = require("html-webpack-partials-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+
   mode: "development",
   devServer: {
     static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
+      filename: "index.html",
       template: "./src/index.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/views/partials/game.html",
+      filename: "game.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+      },
     }),
   ],
   output: {
